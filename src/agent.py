a28fb9c1dfd6b44bf72b42b6970177c2c1c97804
FlorDeBruyne <flor.debruyne@gmail.com>
2026-06-12 14:04:40 +0200

    Added logger to see llm results

--- src/agent.py
+++ src/agent.py
@@ -2,11 +2,15 @@ import asyncio
 from health_client import get_morning_context, get_evening_context, get_workout_context
 from llm_client import main
 from telegram_client import send_message
+import logging
+
+logger = logging.getLogger(__name__)
 
 async def morning_update():
     context = await get_morning_context()
     llm_response = await main(message="Stuur het ochtend bericht op basis van de health context.",
                               context=context)
+    logger.info(llm_response)
     await send_message(llm_response)
 
 
