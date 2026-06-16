b5bf1260cb4973fa00d0ef073dc3ccefab939508
FlorDeBruyne <flor.debruyne@gmail.com>
2026-06-17 00:27:43 +0200

    Wrong optional input in the function

--- src/users/onboarding.py
+++ src/users/onboarding.py
@@ -2,6 +2,6 @@ import sqlalchemy
 from typing import Optional
 __all__ = ['check_onboarding']
 
-async def check_onboarding(telegram_chat_id: Optional[str | None], user_id: Optional[str | None]) -> bool:
+async def check_onboarding(telegram_chat_id: str | None = None, user_id: str | None = None) -> bool:
 
     return False
